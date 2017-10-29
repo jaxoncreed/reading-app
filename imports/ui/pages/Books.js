645bbc3350d520a9ca8d8a8b3f57e92548faed74
Jackson Morgan <jaxoncreed@gmail.com>
2017-10-28 23:23:24 -0400

    Formatted the front end books page not really lol

--- imports/ui/pages/Books.js
+++ imports/ui/pages/Books.js
@@ -3,12 +3,75 @@ import { Meteor } from 'meteor/meteor';
 import container from '../../modules/container';
 import AssignmentSet from '../../api/AssignmentSet';
 
-const Books = ({ assignmentSet }) => (
-  <div className="Books">
-    <h4 className="page-header">Books</h4>
-    <pre>{JSON.stringify(assignmentSet, null, 2)}</pre>
-  </div>
-);
+const Books = ({ assignmentSet }) => {
+
+  // [
+  //   {
+  //     recommender: "frizzle",
+  //     assignments: [
+  //       {
+  //         book: "skdjfkdlsfj",
+  //         students: [blah]
+  //       }
+  //     ]
+  //   }
+  // ]
+
+  const recommenderMap = {};
+
+  assignmentSet.forEach((assignmentPairing) => {
+    const student = assignmentPairing.username;
+    assignmentPairing.assignments.forEach((assignment) => {
+      if (recommenderMap[assignment.recommender]) {
+        if (recommenderMap[assignment.recommender].assignments[assignment.book]) {
+          recommenderMap[assignment.recommender]
+              .assignments[assignment.book].students.push(student);
+        } else {
+          recommenderMap[assignment.recommender].assignment[assignment.book] = {
+            book: assignment.book,
+            students: [student],
+          }
+        }
+      } else {
+        recommenderMap[assignment.recommender] = {
+          recommender: assignment.recommender,
+          assignments: {
+            [assignment.book]: {
+              book: assignment.book,
+              students: [student],
+            },
+          },
+        };
+      }
+    });
+  });
+  console.log(JSON.stringify(recommenderMap, null, 2));
+
+  return (
+    <div className="Books">
+      <h4 className="page-header">Books</h4>
+      {Object.values(recommenderMap).map((recommenderAssignmentPair) => (
+        <div>
+          <h1>{recommenderAssignmentPair.recommender}</h1>
+          {Object.values(recommenderAssignmentPair.assignments).map((assignment) => {
+            return (<p>{assignment.book} {assignment.students.join(', ')}</p>);
+          })}
+        </div>
+      ))}
+    </div>
+  );
+
+  // return (
+  //   <div className="Books">
+  //     <h4 className="page-header">Books</h4>
+  //     {Object.values(recommenderMap).map((recommenderAssignmentPair) => {
+  //       return (
+
+  //       );
+  //     })}
+  //   </div>
+  // );
+};
 
 export default container((props, onData) => {
   const user = Meteor.user();
