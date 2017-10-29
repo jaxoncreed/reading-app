77f1111fdf7c497ffae38bc07f59c3a5afbe361a
Jackson Morgan <jaxoncreed@gmail.com>
2017-10-29 02:39:22 -0400

    Records statistics properly

--- imports/api/BookStat.js
+++ imports/api/BookStat.js
@@ -4,4 +4,47 @@ import { check } from 'meteor/check';
 
 const BookStat = new Mongo.Collection('book_stat');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('bookStat', (username, book) => {
+    check(username, String);
+    check(book, String);
+    const bookStat = BookStat.find({ username, book });
+    if (bookStat) {
+      return bookStat;
+    }
+    const toInsert = {
+      username,
+      book,
+      readingSessions: [],
+    };
+    BookStat.insert(toInsert);
+    return BookStat.find({ username, book });
+  });
+}
+
+Meteor.methods({
+  startSession: (bookStat) => {
+    check(bookStat, Object);
+    let beginPercent = 0;
+    if (bookStat.readingSessions[bookStat.readingSessions.length - 1] &&
+          bookStat.readingSessions[bookStat.readingSessions.length - 1].endPercent) {
+      beginPercent = bookStat.readingSessions[bookStat.readingSessions.length - 1].endPercent;
+    }
+    BookStat.update({ _id: bookStat._id },
+        { $push: { readingSessions: { begin: Date.now(), beginPercent } } });
+  },
+
+  endSession: (bookStat, endPercent) => {
+    check(bookStat, Object);
+    check(endPercent, Number);
+    BookStat.update({ _id: bookStat._id }, {
+      $set: {
+        ['readingSessions.' + (bookStat.readingSessions.length - 1) + '.end']: Date.now(),
+        ['readingSessions.' + (bookStat.readingSessions.length - 1) + '.endPercent']: endPercent,
+      },
+    });
+  },
+});
+
 export default BookStat;
--- imports/ui/pages/Read.js
+++ imports/ui/pages/Read.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import container from '../../modules/container';
 import Book from '../../api/Book';
+import BookStat from '../../api/BookStat';
 
-const Read = ({ book }) => {
-  return (
-    <div className="Books">
-      <h4 className="page-header">{book.title}</h4>
-      <pre>{book.body.body}</pre>
-    </div>
-  );
+import throttle from 'lodash.throttle';
 
-  // return (
-  //   <div className="Books">
-  //     <h4 className="page-header">Books</h4>
-  //     {Object.values(recommenderMap).map((recommenderAssignmentPair) => {
-  //       return (
+class Read extends Component {
 
-  //       );
-  //     })}
-  //   </div>
-  // );
-};
+  componentDidMount() {
+    this.throttle = throttle(() => {
+      const h = document.documentElement;
+      const b = document.body;
+      const st = 'scrollTop';
+      const sh = 'scrollHeight';
+      const percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+      Meteor.call('endSession', this.props.bookStat, percent);
+    }, 1000);
+    window.addEventListener('scroll', this.throttle);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.bookStat && nextProps.bookStat) {
+      Meteor.call('startSession', nextProps.bookStat);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.throttle);
+  }
+
+
+  render() {
+    return (
+      <div className="Books">
+        <h4 className="page-header">{this.props.book.title}</h4>
+        <pre>{this.props.book.body.body}</pre>
+      </div>
+    );
+  }
+}
 
 export default container((props, onData) => {
 
   const subscription = Meteor.subscribe('bookBody', props.params.bookId);
-
   if (subscription.ready()) {
     const book = Book.findOne({ _id: props.params.bookId });
-    console.log(book);
-    onData(null, { book });
+
+    const user = Meteor.user();
+    if (user) {
+      const bookStatSubscription = Meteor.subscribe('bookStat', user.profile.username, props.params.bookId);
+      if (bookStatSubscription) {
+        const bookStat = BookStat.findOne({ username: user.profile.username, book: props.params.bookId });
+        onData(null, { book, bookStat, username: user.profile.username });
+      }
+    }
   }
 }, Read);
